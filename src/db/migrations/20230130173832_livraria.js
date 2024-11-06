ed1a3f9f65d34274526c55d0464a7b7a760afda7
Jonas Santos <jonassoares.live@gmail.com>
2024-11-06 12:30:13 -0300

    fix: :bug: fix: lint error

--- src/db/migrations/20230130173832_livraria.js
+++ src/db/migrations/20230130173832_livraria.js
@@ -3,31 +3,31 @@
  * @returns { Promise<void> }
  */
 export function up(knex) {
-	return knex.schema
-		.createTable("autores", (table) => {
-			table.increments("id");
-			table.string("nome", 255).notNullable();
-			table.string("nacionalidade", 255).notNullable();
-			table.timestamp("created_at").defaultTo(knex.fn.now());
-			table.timestamp("updated_at").defaultTo(knex.fn.now());
-		})
-		.createTable("editoras", (table) => {
-			table.increments("id");
-			table.string("nome", 255).notNullable();
-			table.string("cidade", 255).notNullable();
-			table.string("email", 255).notNullable();
-			table.timestamp("created_at").defaultTo(knex.fn.now());
-			table.timestamp("updated_at").defaultTo(knex.fn.now());
-		})
-		.createTable("livros", (table) => {
-			table.increments("id");
-			table.string("titulo", 255).notNullable();
-			table.integer("paginas").notNullable();
-			table.integer("editora_id").notNullable();
-			table.integer("autor_id").notNullable();
-			table.timestamp("created_at").defaultTo(knex.fn.now());
-			table.timestamp("updated_at").defaultTo(knex.fn.now());
-		});
+  return knex.schema
+    .createTable('autores', (table) => {
+      table.increments('id');
+      table.string('nome', 255).notNullable();
+      table.string('nacionalidade', 255).notNullable();
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
+    })
+    .createTable('editoras', (table) => {
+      table.increments('id');
+      table.string('nome', 255).notNullable();
+      table.string('cidade', 255).notNullable();
+      table.string('email', 255).notNullable();
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
+    })
+    .createTable('livros', (table) => {
+      table.increments('id');
+      table.string('titulo', 255).notNullable();
+      table.integer('paginas').notNullable();
+      table.integer('editora_id').notNullable();
+      table.integer('autor_id').notNullable();
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
+    });
 }
 
 /**
@@ -35,10 +35,10 @@ export function up(knex) {
  * @returns { Promise<void> }
  */
 export function down(knex) {
-	return knex.schema
-		.dropTable("livros")
-		.dropTable("editoras")
-		.dropTable("autores");
+  return knex.schema
+    .dropTable('livros')
+    .dropTable('editoras')
+    .dropTable('autores');
 }
 
 // npx knex --knexfile=./src/db/knexfile.js migrate:make livraria
