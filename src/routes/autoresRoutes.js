f59b58dac2e1765a14407cb1ca42b4df10beab69
Jonas Santos <jonassoares.live@gmail.com>
2024-11-06 12:24:55 -0300

    feat: :sparkles: add: methods to list books by authors

--- src/routes/autoresRoutes.js
+++ src/routes/autoresRoutes.js
@@ -1,13 +1,15 @@
-import express from 'express';
-import AutoresController from '../controllers/autoresController.js';
+import express from "express";
+import AutoresController from "../controllers/autoresController.js";
+import knex from "knex";
 
 const router = express.Router();
 
 router
-  .get('/autores', AutoresController.listarAutores)
-  .get('/autores/:id', AutoresController.listarAutorPorId)
-  .post('/autores', AutoresController.cadastrarAutor)
-  .put('/autores/:id', AutoresController.atualizarAutor)
-  .delete('/autores/:id', AutoresController.excluirAutor);
+	.get("/autores", AutoresController.listarAutores)
+	.get("/autores/:id", AutoresController.listarAutorPorId)
+	.get("/autores/:id/livros", AutoresController.listarLivrosPorAutor)
+	.post("/autores", AutoresController.cadastrarAutor)
+	.put("/autores/:id", AutoresController.atualizarAutor)
+	.delete("/autores/:id", AutoresController.excluirAutor);
 
 export default router;
